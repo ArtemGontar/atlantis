20282a9ecf32699a8bd0c0af5f41aa1151d906a3
Artem Hontar iMac <artsiom.hontar@ihsmarkit.com>
2022-02-26 10:36:19 +0400

    feat: init 2

--- contracts/AtlantisToken.sol
+++ contracts/AtlantisToken.sol
@@ -1,1 +1,11 @@
 // SPDX-Licence-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract AtlantisToken is ERC20 {
+  constructor(uint256 initSupply) ERC20("Atlantis", "ATL"){
+    _mint(msg.sender, initSupply);
+  }
+}
