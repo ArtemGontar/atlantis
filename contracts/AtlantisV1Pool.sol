daeaec7d6c59271a14eb8327dcbab72d68531953
Artem Hontar iMac <artsiom.hontar@ihsmarkit.com>
2022-03-03 18:03:03 +0400

    feat: add Factory

---
+++ contracts/AtlantisV1Pool.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import './interfaces/IAtlantisV1Pool.sol';
+import './NoDelegateCall.sol';
+
+contract AtlantisV1Pool is IAtlantisV1Pool, NoDelegateCall {
+  
+}
---
+++ contracts/interfaces/IAtlantisV1Pool.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/// @title The interface for a Atlantis V1 Pool
+/// @notice A Atlantis pool facilitates swapping and automated market making between any two assets that strictly conform
+/// to the ERC20 specification
+/// @dev The pool interface is broken up into many smaller pieces
+interface IAtlantisV1Pool
+{
+
+}
